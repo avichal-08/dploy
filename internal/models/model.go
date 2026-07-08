613d6db9a25f7a103e48f60c50c7bd616f58bb7d
Avichal Pandey <avichal2018lko@gmail.com>
2026-07-08 16:42:17 +0530

    project init

---
+++ internal/models/model.go
@@ -0,0 +1,39 @@
+package models
+
+import (
+	"time"
+)
+
+type User struct {
+	ID        string    `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	GitHubID  string    `gorm:"type:varchar(255);uniqueIndex;not null"`
+	Email     string    `gorm:"type:varchar(255);uniqueIndex;not null"`
+	AvatarURL string    `gorm:"type:text"`
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+
+	Projects []Project `gorm:"constraint:OnDelete:CASCADE;"`
+}
+
+type Project struct {
+	ID            string    `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	UserID        string    `gorm:"type:uuid;not null;uniqueIndex:idx_user_project"`
+	Name          string    `gorm:"type:varchar(255);not null;uniqueIndex:idx_user_project"`
+	RepositoryURL string    `gorm:"type:text;not null"`
+	Framework     string    `gorm:"type:varchar(50)"`
+	ProductionURL string    `gorm:"type:text"`
+	CreatedAt     time.Time `gorm:"autoCreateTime"`
+
+	Deployments []Deployment `gorm:"constraint:OnDelete:CASCADE;"`
+}
+
+type Deployment struct {
+	ID           string `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	ProjectID    string `gorm:"type:uuid;not null"`
+	CommitSHA    string `gorm:"type:varchar(40);not null"`
+	Status       string `gorm:"type:varchar(50);default:'pending';not null"`
+	ContainerID  string `gorm:"type:varchar(255)"`
+	InternalPort int
+	BuildLogs    string    `gorm:"type:text"`
+	CreatedAt    time.Time `gorm:"autoCreateTime"`
+	FinishedAt   *time.Time
+}
